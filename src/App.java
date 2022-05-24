d9d0fd3f8b625f42a1274750af5280bbc4a78b32
gregr <greg.rodriguez@outlook.com>
2022-05-23 21:26:31 -0500

    added GUI for user input

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //GUI user input for loan amount
+        int principal = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter loan amount. "));
+        
+        //GUI user input for interest rate
+        double interest = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter interest rate. "));
+        
+        //GUI user input for length of loan in years
+        int years = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter loan term in years. "));
+        
     }
 }
